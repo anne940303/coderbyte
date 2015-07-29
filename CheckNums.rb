089c5cd8e7598dd467ea5d4930518ec503c67843
Sooahn Shin <anne940303@gmail.com>
2015-07-29 19:57:10 +0900

    Create CheckNums.rb

---
+++ CheckNums.rb
@@ -0,0 +1,14 @@
+def CheckNums(num1,num2)
+
+  # code goes here
+  checknum = true
+  if num1<num2
+    checknum = true
+  elsif num1>num2
+    checknum = false
+  elsif num1==num2
+    checknum=-1
+  end
+    return checknum 
+         
+end
